eab9a24db449e30e89886807bbed4be861203064
Surya Vivek <51923160+vivek-cyber@users.noreply.github.com>
2023-04-26 15:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function threeSum(arr, target) {
-// write your code here
-  
+	let close=Number.MAX_VALUE
+for(let i=0;i<arr.length;i++)
+	{
+		for(let j=i+1;j<arr.length;j++)
+			{
+				for(let k=0;k<arr.length;k++)
+					{
+						if (Math.abs(target - close) >Math.abs(x - (arr[i] + arr[j] + arr[k])))
+                    close= (arr[i] + arr[j] + arr[k]);
+					}
+			}
+	}
+	return close
 }
 
 module.exports = threeSum;
